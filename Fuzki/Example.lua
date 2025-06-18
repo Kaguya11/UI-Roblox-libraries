58ae7884081d6d5f8f028565dc5f7534f402e460
Kaguyaa1 <106523037+Kaguya11@users.noreply.github.com>
2025-06-18 18:10:44 +0500

    Create Example.lua

---
+++ Fuzki/Example.lua
@@ -0,0 +1,21 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Fuzki-UI-Library/main/FuzkiLibrary.lua"))() -- Gets the Library
+local Main = Library:Create("Fuzki", "Baseplate") -- Creates Main Window | Fuzki (Hub Name) | Baseplate (Game Name)
+
+local Preview = Main:CreateSection("Preview")
+
+Preview:CreateLabel("Text Label") -- Text Label with text inside of " "
+Preview:CreateButton("Button Text", "Button Info", function() -- creates button (CreateButton)
+   print("Wow, printed")
+end)
+Preview:CreateToggle("Toggle Info", function(state) -- creates Toggle
+   print(state) -- prints state (true or false)
+end)
+Preview:CreateBind("Keybind Info", Enum.KeyCode.F, function() -- Enum.KeyCode.F is starting KeyBind
+   print("Keybind Was Pressed")
+end)
+Preview:CreateTextBox("TextBox Info", "PlaceHolder", function(v) -- v= gets textbox text after enter pressed
+print(v)
+end)
+Preview:CreateSlider(16, 500, "Slider Info", function(val) -- 16 (Min value) | 500 (Max value)
+print(val) -- prints value 
+end)
